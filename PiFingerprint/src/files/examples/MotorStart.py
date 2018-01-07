6009a70882a847a218d1671fec6ae4bbee82272a
Adam Kelly <adam.mark.kelly@gmail.com>
2018-01-07 22:56:06 +0000

    Repos Added (Fingerprint Reader & MagicMirror)

---
+++ PiFingerprint/src/files/examples/MotorStart.py
@@ -0,0 +1,6 @@
+def motor_start():
+	print 'Motors Starting...'
+
+if __name__ == '__main__':
+	##perform motor_start
+	motor_start()
---
+++ PiFingerprint/src/files/examples/example_search.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+PyFingerprint
+Copyright (C) 2015 Bastian Raschke <bastian.raschke@posteo.de>
+All rights reserved.
+
+"""
+
+import hashlib
+import MotorStart
+from pyfingerprint.pyfingerprint import PyFingerprint
+## Global Var for loop here
+inLoop = 0
+## Locked/Unlocked identification var
+Unlocked = None
+
+## Search for a finger
+##
+
+## Tries to initialize the sensor
+try:
+    f = PyFingerprint('/dev/ttyUSB0', 57600, 0xFFFFFFFF, 0x00000000)
+
+    if ( f.verifyPassword() == False ):
+        raise ValueError('The given fingerprint sensor password is wrong!')
+
+except Exception as e:
+    print('The fingerprint sensor could not be initialized!')
+    print('Exception message: ' + str(e))
+    exit(1)
+
+## Gets some sensor information
+print('Currently used templates: ' + str(f.getTemplateCount()) +'/'+ str(f.getStorageCapacity()))
+
+## Tries to search the finger and calculate hash
+## Start loop here
+while (inLoop == 0):
+    print 'Loop start'
+    try:
+        print('Waiting for finger...')
+
+        ## Wait that finger is read
+        while ( f.readImage() == False ):
+            pass
+
+        ## Converts read image to characteristics and stores it in charbuffer 1
+        f.convertImage(0x01)
+
+        ## Searchs template
+        result = f.searchTemplate()
+
+        positionNumber = result[0]
+        accuracyScore = result[1]
+
+        if ( positionNumber == -1 ):
+            print('No match found!')
+            ##exit(0)
+        else:
+            print('Found template at position #' + str(positionNumber))
+            print('The accuracy score is: ' + str(accuracyScore))
+            ##Escape for loop here and call motorstart.py
+	    Unlocked = True
+	    if Unlocked == True:
+		##Open MotorStart.py but for now test with a test unit.
+		MotorStart.motor_start()
+	    inLoop = 1
+	    print 'Loop finish'
+
+        ## OPTIONAL stuff
+        ##
+
+        ## Loads the found template to charbuffer 1
+        f.loadTemplate(positionNumber, 0x01)
+
+        ## Downloads the characteristics of template loaded in charbuffer 1
+        characterics = str(f.downloadCharacteristics(0x01)).encode('utf-8')
+
+        ## Hashes characteristics of template
+        print('SHA-2 hash of template: ' + hashlib.sha256(characterics).hexdigest())
+
+    except Exception as e:
+        print('Operation failed!')
+        print('Exception message: ' + str(e))
+        ##exit(1)
